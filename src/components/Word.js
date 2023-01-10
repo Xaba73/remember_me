e51ee141f6b2197176a91c457f8ba14f9d1fb2b2
Xaba73 <101169583+Xaba73@users.noreply.github.com>
2023-01-10 13:10:08 +0300

    feature: реализовал логику показа карточек

--- src/components/Word.js
+++ src/components/Word.js
@@ -5,13 +5,17 @@ const DUMMY_WORD = [
   { russian: 'машина', english: 'car' },
   { russian: 'красный', english: 'red' },
   { russian: 'медведь', english: 'bear' },
+  { russian: 'Явно', english: 'explicitly' },
+  { russian: 'Оказывается', english: 'it turns out' },
+  { russian: 'уязвимый', english: 'vulnerable' },
+  { russian: 'Хотя, однако', english: 'though' },
+  { russian: 'Довольно, вполне', english: 'quite' },
 ];
 
 function arrayShuffle(array) {
   if (!Array.isArray(array)) {
     throw new TypeError(`Expected an array, got ${typeof array}`);
   }
-
   array = [...array];
 
   for (let index = array.length - 1; index > 0; index--) {
@@ -24,19 +28,39 @@ function arrayShuffle(array) {
 
 const Word = () => {
   const [wordArray, setWordArray] = useState(DUMMY_WORD);
+  const [showAnswer, setShowAnswer] = useState(false);
 
   const shuffleWordHandler = () => {
     setWordArray(arrayShuffle(DUMMY_WORD));
+    setShowAnswer(!showAnswer);
+  };
+  const revealAnswerHandler = () => {
+    setShowAnswer(!showAnswer);
   };
 
+  let wordButton = (
+    <button className={styles.button} onClick={shuffleWordHandler}>
+      Next
+    </button>
+  );
+  if (showAnswer === false) {
+    wordButton = (
+      <button className={styles.button} onClick={revealAnswerHandler}>
+        Reveal answer
+      </button>
+    );
+  }
   return (
     <div className={styles.container}>
       <header></header>
       <div className={styles.wrapper}>
         <p className={styles.word_main}>{wordArray[0].russian}</p>
-        <p className={styles.word_main}>{wordArray[0].english}</p>
+        {showAnswer && (
+          <p className={styles.word_main}>{wordArray[0].english}</p>
+        )}
       </div>
-      <button onClick={shuffleWordHandler}>Next</button>
+      {showAnswer && wordButton}
+      {!showAnswer && wordButton}
     </div>
   );
 };
